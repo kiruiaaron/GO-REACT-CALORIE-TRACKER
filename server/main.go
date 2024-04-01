ef1065bf6c964fbcc0d66cfe9855e70f77b900bb
kiruiaaron <kiruiaaron471@gmail.com>
2024-04-01 13:35:56 +0300

    updated the addition of cors

--- server/main.go
+++ server/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/kiruiaaron/GO-REACT-CALORIE-TRACKER/middlewares"
 	"github.com/kiruiaaron/GO-REACT-CALORIE-TRACKER/routes"
 )
 
+func main() {
 
-func main(){
 	port := os.Getenv("PORT")
-	if port == ""{
+	if port == "" {
 		port = "8000"
 	}
 
@@ -19,11 +20,13 @@ func main(){
 	router.Use(gin.Logger())
 	router.Use(cors.Default())
 
+	router.Use(middlewares.CORSMiddleware())
+
 	router.POST("/entry/create", routes.AddEntry)
-	router.GET("/entries",routes.GetEntries)
-	router.GET("entry/:id",routes.GetEntryById)
-	router.GET("/ingredient/:ingredient",routes.GetEntryByIngredient)
-	router.PUT("/entry/update/:id",routes.UpdateEntry)
+	router.GET("/entries", routes.GetEntries)
+	router.GET("entry/:id", routes.GetEntryById)
+	router.GET("/ingredient/:ingredient", routes.GetEntryByIngredient)
+	router.PUT("/entry/update/:id", routes.UpdateEntry)
 	router.PUT("/ingredient/update/:id", routes.UpdateIngredient)
 	router.DELETE("/entry/delete/:id", routes.DeleteEntry)
 	router.Run(":" + port)
